86a46860cc0bc721e1e55e851d6b868891623f4b
Hakim Benkacem <hbenkacem@yahoo.co.uk>
2023-03-21 14:34:52 +0400

    adding new quiz page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "/pages/dashboard", to: "pages#dashboard"
 
-
+  resources :quizzes, only: [:create, :new, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
