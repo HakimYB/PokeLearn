f70a4cd4f04abad1745f93505e71abbcd6d343d3
Ayano Umemoto <umemoto.ayano@gmail.com>
2023-03-21 12:43:36 +0100

    added username to sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get "/pages/dashboard", to: "pages#dashboard"
+  get "/dashboard", to: "pages#dashboard"
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
