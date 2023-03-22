ea0a2d155b7d44f83cad6b2bf51cc0b5c5ca0d6e
francois vallat <vallatfrancois@yahoo.fr>
2023-03-22 10:49:26 +0100

    new

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_140635) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_160431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pokemons", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "pokemon_type"
     t.text "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
