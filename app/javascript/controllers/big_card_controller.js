fe278f046189d754323d791a6f919e13e2d3547f
Ayano Umemoto <umemoto.ayano@gmail.com>
2023-03-28 00:14:07 +0200

    text-decoration:none on the dex cards, adjust layouts on big card, added target of stimulus again because I seem to have deleted it.

--- app/javascript/controllers/big_card_controller.js
+++ app/javascript/controllers/big_card_controller.js
@@ -2,21 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="big-card"
 export default class extends Controller {
-  static targets = ["image", "name", "types", "details", "card"]
+  static targets = ["card"]
   connect() {
     console.log("hey");
-    console.log(this.element)
-    console.log(this.imageTarget)
-    console.log(this.nameTarget)
-    console.log(this.typesTarget)
-    console.log(this.detailsTarget)
+    console.log(this.cardTarget)
   }
 
   showBigCard(event) {
     event.preventDefault()
     console.log("helloooooo")
     // console.log(event.currentTarget)
-    console.log(this.imageTarget)
     fetch(event.currentTarget.href, {headers: {"Accept": "text/plain"}})
     .then(response => response.text())
     .then((data) => {
