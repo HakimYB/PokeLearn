48cf9341f79290b4f0c0cb6287edccaa2ebd4245
Ayano Umemoto <umemoto.ayano@gmail.com>
2023-03-27 14:25:50 +0200

    adding big-card stimulus targets

--- app/javascript/controllers/big_card_controller.js
+++ app/javascript/controllers/big_card_controller.js
@@ -2,7 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="big-card"
 export default class extends Controller {
+  static targets = ["image", "name", "types", "details"]
   connect() {
     console.log("hey");
+    console.log(this.element)
+    console.log(this.imageTarget)
+    console.log(this.nameTarget)
+    console.log(this.typesTarget)
+    console.log(this.detailsTarget)
+  }
+
+  showBigCard(event) {
+    event.preventDefault()
+    this.imageTarget.src = "<%= this.eventTarget.image_url"
   }
 }
