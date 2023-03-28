3a8d18aedffbeecd6a7438e6bf8685d3fd9bef1d
abdullah alameen <ando.jaber@gmail.com>
2023-03-28 12:09:20 +0300

    finished

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -3,7 +3,6 @@ import { Tooltip } from "bootstrap/dist/js/bootstrap.bundle.js";
 
 export default class extends Controller {
   connect() {
-    console.log(this.element)
     new Tooltip(this.element)
   }
 }
